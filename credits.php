b48fd15f2c9ba2782bb383b3b5a8f2e267961c7c
Diana <109088720+Diana-Kaba@users.noreply.github.com>
2023-11-28 10:02:18 +0200

    Update credits.php

--- credits.php
+++ credits.php
@@ -1,7 +1,7 @@
 <?php
 
 //полученный ранее токен
-$token = "6824229964:AAHq61ZI_rs8eo9yu31UkhdrtkS4rQsvaE0";
+$token = "";
 //полученный ранее chat_id
-$chat_id = "985668861";
-$group_chat_id = "-1002037939143";
+$chat_id = "";
+$group_chat_id = "";
